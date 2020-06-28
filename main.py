e4268d6e6cd2b7adcde23bd7ce52acebb7126de4
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-06-28 16:47:38 +0530

    removes redundant comments

--- main.py
+++ main.py
@@ -36,12 +36,7 @@ def isVowel(character):
 
 
 def createVisibleAlphabets(word):
-    # alphabets = ''
-    # for character in word:
-    #    alphabets += character if isVowel(character) else '_'
-    # return alphabets
-
-    return ''.join([word[i] if isVowel(word[i]) else '_' for i in range(len(word))])
+    return ''.join([character if isVowel(character) else '_' for character in word])
 
 
 def startGame(attempts=5):
